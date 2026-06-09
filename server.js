cb11fc994af639dc827ef108e3798aefa073351d
utsavbamroliya09 <utsavbamroliya09@gmail.com>
2026-06-09 17:10:11 +0530

    change

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chatweb-1b275.web.app",
     methods: ["GET", "POST"],
   },
 });
